0e2dd0601457bf66e61dc15316c817c7fd6420f7
Youssef <yakhazzan04@gmail.com>
2026-05-08 15:10:55 +0200

    Primer commit: joc d'endevinar el número

---
+++ JocEndevinaNumero.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JocEndevinaNumero {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner teclat = new Scanner(System.in);
+
+        int numero = random.nextInt(50) + 1;
+
+        System.out.print("Endevina el número (1-50): ");
+        int usuari = teclat.nextInt();
+
+        if (usuari == numero) {
+            System.out.println("L'has encertat!");
+        } else {
+            System.out.println("No l'has encertat.");
+            System.out.println("El número era: " + numero);
+        }
+
+        teclat.close();
+    }
+}
